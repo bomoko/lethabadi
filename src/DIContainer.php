908e86a2b4ab0ac146bb372c72b535ded49e6ce9
bomoko <blaize.kaye@gmail.com>
2017-09-21 15:55:09 +1200

    added singleton binding

--- src/DIContainer.php
+++ src/DIContainer.php
@@ -1,15 +1,48 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bomoko
- * Date: 2017/09/21
- * Time: 2:57 PM
- */
-
-namespace Bomoko\Lethaba;
-
+<?php namespace Bomoko\Lethaba;
 
 class DIContainer
 {
 
+    protected $values = [];
+
+    public function __construct(array $values = [])
+    {
+        $this->values = $values;
+    }
+
+    public function __invoke($key)
+    {
+        if (!key_exists($key, $this->values)) {
+            throw new \InvalidArgumentException(sprintf("The key '%s' has not been bound in the container",
+              $key));
+        }
+
+        //anything that's callable, gets called
+        if (is_callable($this->values[$key])) {
+            return $this->values[$key]($this);
+        }
+
+        return $this->values[$key];
+    }
+
+    public function bind($key, $value)
+    {
+        $this->values[$key] = $value;
+    }
+
+    public function bindSingleton($key, $value)
+    {
+        if (!is_callable($value)) {
+            throw new \InvalidArgumentException("Singletons have to be invokable");
+        }
+
+        $this->values[$key] = function ($c) use ($value) {
+            static $singleton;
+
+            if(is_null($singleton)) {
+                $singleton = $value($c);
+            }
+            return $singleton;
+        };
+    }
 }
--- tests/DIContainerTest.php
+++ tests/DIContainerTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bomoko
- * Date: 2017/09/21
- * Time: 2:59 PM
- */
 
 use Bomoko\Lethaba\DIContainer;
 use PHPUnit\Framework\TestCase;
@@ -18,5 +12,76 @@ class DIContainerTest extends TestCase
         $this->assertInstanceOf(DIContainer::class, $container);
     }
 
-    
+
+    /** @test */
+    public function it_should_accept_an_array_of_parameters_at_creation_time()
+    {
+        $container = new DIContainer(['a' => 'abc', 'b' => 123]);
+        $this->assertEquals('abc', $container('a'));
+        $this->assertEquals(123, $container('b'));
+    }
+
+    /** @test */
+    public function it_should_allow_us_to_add_parameters_once_instantiated()
+    {
+        $container = new DIContainer();
+        $container->bind('a', 'abc');
+        $this->assertEquals('abc', $container('a'));
+    }
+
+    /** @test */
+    public function it_should_throw_an_error_when_a_key_doesnt_exist()
+    {
+        $container = new DIContainer();
+        $this->expectException(InvalidArgumentException::class);
+        $output = $container('notBoundValue');
+    }
+
+    /** @test */
+    public function it_should_run_an_invokable_passed_directly_into_bind()
+    {
+        $container = new DIContainer();
+        $container->bind('invokable', function ($c) {
+            return 'invoked';
+        });
+        $this->assertEquals('invoked', $container('invokable'));
+    }
+
+    /** @test */
+    public function it_should_allow_invoked_functions_access_to_the_container_itself()
+    {
+        $container = new DIContainer();
+        $container->bind('toBeAccessed', 'accessed');
+        $container->bind('invoked', function ($c) {
+          return $c('toBeAccessed');
+        });
+
+        $this->assertEquals('accessed', $container('invoked'));
+    }
+
+    /** @test */
+    public function it_should_return_different_objects_on_each_invocation_if_service_not_defined_as_singleton()
+    {
+        $container = new DIContainer();
+        $container->bind('service', function ($c) {
+            return new stdClass();
+        });
+
+        $firstInvocation = $container('service');
+        $secondInvocation = $container('service');
+        $this->assertFalse($firstInvocation === $secondInvocation);
+    }
+
+    /** @test */
+    public function it_should_bind_a_singleton_and_return_the_same_object_on_every_invocation()
+    {
+        $container = new DIContainer();
+        $container->bindSingleton('singleton', function ($c) {
+            return new stdClass();
+        });
+
+        $firstInvocation = $container('singleton');
+        $secondInvocation = $container('singleton');
+        $this->assertTrue($firstInvocation === $secondInvocation);
+    }
 }
